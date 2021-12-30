d0ee3f8bf7a5c012f29ceee2ec4d8d34c774ea5a
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-12-29 17:05:40 -0800

    atributes.c and makefiles

--- tmx/hid-tmx/attributes.c
+++ tmx/hid-tmx/attributes.c
@@ -1,55 +1,55 @@
-static inline int t150_init_attributes(struct t150 *t150)
+static inline int tmx_init_attributes(struct tmx *tmx)
 {
 	int errno;
 
 	// Before making avaible the syfs attrbiutes we try to set them to some default
 	// @FIXME I do not know if it is desiradable to wait for URBs in probe() method...
-	t150_setup_task(t150);
+	tmx_setup_task(tmx);
 
-	errno = device_create_file(&t150->usb_device->dev, &dev_attr_autocenter);
+	errno = device_create_file(&tmx->usb_device->dev, &dev_attr_autocenter);
 	if(errno)
 		return errno;
 
-	errno = device_create_file(&t150->usb_device->dev, &dev_attr_enable_autocenter);
+	errno = device_create_file(&tmx->usb_device->dev, &dev_attr_enable_autocenter);
 	if(errno)
 		goto err1;
 
-	errno = device_create_file(&t150->usb_device->dev, &dev_attr_range);
+	errno = device_create_file(&tmx->usb_device->dev, &dev_attr_range);
 	if(errno)
 		goto err2;
 
-	errno = device_create_file(&t150->usb_device->dev, &dev_attr_gain);
+	errno = device_create_file(&tmx->usb_device->dev, &dev_attr_gain);
 	if(errno)
 		goto err3;
 
-	errno = device_create_file(&t150->usb_device->dev, &dev_attr_firmware_version);
+	errno = device_create_file(&tmx->usb_device->dev, &dev_attr_firmware_version);
 	if(errno)
 		goto err4;
 
 	return 0;
 
-err4:	device_remove_file(&t150->usb_device->dev, &dev_attr_firmware_version);
-err3:	device_remove_file(&t150->usb_device->dev, &dev_attr_range);
-err2:	device_remove_file(&t150->usb_device->dev, &dev_attr_enable_autocenter);
-err1:	device_remove_file(&t150->usb_device->dev, &dev_attr_autocenter);
+err4:	device_remove_file(&tmx->usb_device->dev, &dev_attr_firmware_version);
+err3:	device_remove_file(&tmx->usb_device->dev, &dev_attr_range);
+err2:	device_remove_file(&tmx->usb_device->dev, &dev_attr_enable_autocenter);
+err1:	device_remove_file(&tmx->usb_device->dev, &dev_attr_autocenter);
 	return errno;
 }
 
-static inline void t150_free_attributes(struct t150 *t150)
+static inline void tmx_free_attributes(struct tmx *tmx)
 {
-	device_remove_file(&t150->usb_device->dev, &dev_attr_autocenter);
-	device_remove_file(&t150->usb_device->dev, &dev_attr_enable_autocenter);
-	device_remove_file(&t150->usb_device->dev, &dev_attr_range);
-	device_remove_file(&t150->usb_device->dev, &dev_attr_gain);
-	device_remove_file(&t150->usb_device->dev, &dev_attr_firmware_version);
+	device_remove_file(&tmx->usb_device->dev, &dev_attr_autocenter);
+	device_remove_file(&tmx->usb_device->dev, &dev_attr_enable_autocenter);
+	device_remove_file(&tmx->usb_device->dev, &dev_attr_range);
+	device_remove_file(&tmx->usb_device->dev, &dev_attr_gain);
+	device_remove_file(&tmx->usb_device->dev, &dev_attr_firmware_version);
 }
 
 /**/
-static ssize_t t150_store_return_force(struct device *dev, struct device_attribute *attr,
+static ssize_t tmx_store_return_force(struct device *dev, struct device_attribute *attr,
 	const char *buf, size_t count)
 {
 	uint8_t nforce;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 
 	// If mallformed input leave...
 	if(kstrtou8(buf, 10, &nforce))
@@ -58,55 +58,55 @@ static ssize_t t150_store_return_force(struct device *dev, struct device_attribu
 	if(nforce > 100)
 		nforce = 100;
 
-	t150_set_autocenter(t150, nforce);
+	tmx_set_autocenter(tmx, nforce);
 
 	return count;
 }
 
-static ssize_t t150_show_return_force(struct device *dev, struct device_attribute *attr,char * buf )
+static ssize_t tmx_show_return_force(struct device *dev, struct device_attribute *attr,char * buf )
 {
 	int len;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 	unsigned long flags;
 
-	spin_lock_irqsave(&t150->settings.access_lock, flags);
-	len = sprintf(buf, "%d\n", t150->settings.autocenter_force);
-	spin_unlock_irqrestore(&t150->settings.access_lock, flags);
+	spin_lock_irqsave(&tmx->settings.access_lock, flags);
+	len = sprintf(buf, "%d\n", tmx->settings.autocenter_force);
+	spin_unlock_irqrestore(&tmx->settings.access_lock, flags);
 
 	return len;
 }
 
-static ssize_t t150_store_simulate_return_force(struct device *dev, struct device_attribute *attr,
+static ssize_t tmx_store_simulate_return_force(struct device *dev, struct device_attribute *attr,
 	const char *buf, size_t count)
 {
 	bool use;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 
 	// If mallformed input leave...
 	if(!kstrtobool(buf, &use))
-		t150_set_enable_autocenter(t150, use);	
+		tmx_set_enable_autocenter(tmx, use);	
 
 	return count;
 }
 
-static ssize_t t150_show_simulate_return_force(struct device *dev, struct device_attribute *attr,char * buf)
+static ssize_t tmx_show_simulate_return_force(struct device *dev, struct device_attribute *attr,char * buf)
 {
 	int len;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 	unsigned long flags;
 
-	spin_lock_irqsave(&t150->settings.access_lock, flags);
-	len = sprintf(buf, "%c\n", t150->settings.autocenter_enabled ? 'y' : 'n');
-	spin_unlock_irqrestore(&t150->settings.access_lock, flags);
+	spin_lock_irqsave(&tmx->settings.access_lock, flags);
+	len = sprintf(buf, "%c\n", tmx->settings.autocenter_enabled ? 'y' : 'n');
+	spin_unlock_irqrestore(&tmx->settings.access_lock, flags);
 
 	return len;
 }
 
-static ssize_t t150_store_range(struct device *dev, struct device_attribute *attr,
+static ssize_t tmx_store_range(struct device *dev, struct device_attribute *attr,
 	const char *buf, size_t count)
 {
 	uint16_t range;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 
 	// If mallformed input leave...
 	if(kstrtou16(buf, 10, &range))
@@ -114,34 +114,34 @@ static ssize_t t150_store_range(struct device *dev, struct device_attribute *att
 
 	if(range < 270)
 		range = 270;
-	else if (range > 1080)
-		range = 1080;
+	else if (range > 900)
+		range = 900;
 
-	range = DIV_ROUND_CLOSEST((range * 0xffff), 1080);
+	range = DIV_ROUND_CLOSEST((range * 0xffff), 900);
 
-	t150_set_range(t150, range);
+	tmx_set_range(tmx, range);
 
 	return count;
 }
 
-static ssize_t t150_show_range(struct device *dev, struct device_attribute *attr,char * buf )
+static ssize_t tmx_show_range(struct device *dev, struct device_attribute *attr,char * buf )
 {
 	int len;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 	unsigned long flags;
 
-	spin_lock_irqsave(&t150->settings.access_lock, flags);
-	len = sprintf(buf, "%d\n", DIV_ROUND_CLOSEST(t150->settings.range * 1080, 0xffff));
-	spin_unlock_irqrestore(&t150->settings.access_lock, flags);
+	spin_lock_irqsave(&tmx->settings.access_lock, flags);
+	len = sprintf(buf, "%d\n", DIV_ROUND_CLOSEST(tmx->settings.range * 900, 0xffff));
+	spin_unlock_irqrestore(&tmx->settings.access_lock, flags);
 
 	return len;
 }
 
-static ssize_t t150_store_ffb_intensity(struct device *dev, struct device_attribute *attr,
+static ssize_t tmx_store_ffb_intensity(struct device *dev, struct device_attribute *attr,
 	const char *buf, size_t count)
 {
 	uint8_t nforce;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 
 	// If mallformed input leave...
 	if(kstrtou8(buf, 10, &nforce))
@@ -152,32 +152,32 @@ static ssize_t t150_store_ffb_intensity(struct device *dev, struct device_attrib
 
 	nforce = DIV_ROUND_CLOSEST(nforce * 0x80, 100);
 
-	t150_set_gain(t150, nforce);
+	tmx_set_gain(tmx, nforce);
 	return count;
 }
 
-static ssize_t t150_show_ffb_intensity(struct device *dev, struct device_attribute *attr,char * buf )
+static ssize_t tmx_show_ffb_intensity(struct device *dev, struct device_attribute *attr,char * buf )
 {
 	int len;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 	unsigned long flags;
 
-	spin_lock_irqsave(&t150->settings.access_lock, flags);
-	len = sprintf(buf, "%d\n", DIV_ROUND_CLOSEST(t150->settings.gain * 100, 0x80));
-	spin_unlock_irqrestore(&t150->settings.access_lock, flags);
+	spin_lock_irqsave(&tmx->settings.access_lock, flags);
+	len = sprintf(buf, "%d\n", DIV_ROUND_CLOSEST(tmx->settings.gain * 100, 0x80));
+	spin_unlock_irqrestore(&tmx->settings.access_lock, flags);
 
 	return len;
 }
 
-ssize_t t150_show_fw_version(struct device *dev, struct device_attribute *attr,char * buf )
+ssize_t tmx_show_fw_version(struct device *dev, struct device_attribute *attr,char * buf )
 {
 	int len;
-	struct t150 *t150 = dev_get_drvdata(dev);
+	struct tmx *tmx = dev_get_drvdata(dev);
 	unsigned long flags;
 
-	spin_lock_irqsave(&t150->settings.access_lock, flags);
-	len = sprintf(buf, "%d\n", t150->settings.firmware_version);
-	spin_unlock_irqrestore(&t150->settings.access_lock, flags);
+	spin_lock_irqsave(&tmx->settings.access_lock, flags);
+	len = sprintf(buf, "%d\n", tmx->settings.firmware_version);
+	spin_unlock_irqrestore(&tmx->settings.access_lock, flags);
 
 	return len;
 }
